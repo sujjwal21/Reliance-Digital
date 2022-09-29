ef051fd70be99c599a65e4c64c15e60adf0a855f
sujjwal21 <sujjwal85@gmail.com>
2022-09-29 22:29:22 +0530

    update

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
-let data=document.querySelector("form").addEventListener("submit",myfun);
+document.querySelector("form").addEventListener("submit",myfun);
+console.log("shyam")
+
 function myfun(event){
     event.preventDefault();
     let search=document.querySelector("#searchBox").value;
     localStorage.setItem("searchItem",search)
+    console.log("ram")
 }
