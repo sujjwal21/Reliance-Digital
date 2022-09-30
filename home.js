d91251158c15b076ec78db9faee89a27551314db
sujjwal21 <sujjwal85@gmail.com>
2022-09-30 14:21:20 +0530

    update

--- home.js
+++ home.js
@@ -7,3 +7,72 @@ function myfun(event){
     localStorage.setItem("searchItem",search)
     console.log("ram")
 }
+
+
+//  let data1=[
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/page/redmi-9-activ-mobiles"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/page/oneplus-tv"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-voltas-beko-refs:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-sparkling-i5-laptops:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-soundbars:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/page/motorola-e32s-mobile"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-sparkling-printers:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-sparkling-mixers:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-sparkling-keyboards-mouse:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-sparkling-water-heaters:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-neckbands:availability:Exclude%20out%20of%20Stock&page=0"
+//   },
+//   {
+//    "image": "https://www.reliancedigital.in/build/client/images/loaders/mdefault.png",
+//    "url": "https://www.reliancedigital.in/page/tecno-pop-5-mobiles"
+//   }
+//  ]
+
+// function display(data){
+//     data.forEach(function(el){
+//         let div=document.createElement("div")
+
+//         let an=document.createElement("a")
+//         an.url=el.url
+
+//         let img=document.createElement("img")
+//         img.src=el.image
+
+//         an.append(img)
+//         div.append(an)
+//         document.querySelector(".top2").append(div)
+//     })
+// }
+// display(data)
