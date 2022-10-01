004b7e6a6d98100067f9c8330fb071c878b87cab
sujjwal21 <sujjwal85@gmail.com>
2022-10-01 13:57:47 +0530

    new page

---
+++ discount.js
@@ -0,0 +1,151 @@
+let data=[{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t1.png?context=bWFzdGVyfHJvb3R8MjEyMTUzfGltYWdlL3BuZ3xoYjgvaDIzLzk4OTU0MjgwNjMyNjIucG5nfGFiNDA3NWE5YzI2MDhhZjQ3MTcyNDZlYjNkYTNkMmYwMzMxZmYzMTg3YjAwYjFhYWQyZjY1NGQ1ZmQzNjU4YTk",
+url:"https://www.reliancedigital.in/page/redmi-9-activ-mobiles"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t2.png?context=bWFzdGVyfHJvb3R8MTU2NzA3fGltYWdlL3BuZ3xoNzQvaGQxLzk4OTU0MjgwOTYwMzAucG5nfGI2ZDY5NGM2YWYzY2M2Y2E2NzE1ODU4YjVhYjNmNGNjNDgzNGVkMzFlZDZmMTE2ZmMzMzJjNzIyYTA5Y2Y0MTE",
+url:"https://www.reliancedigital.in/refrigerators/c/S101513?searchQuery=:relevance:availability:Exclude%20out%20of%20Stock:101048_03_Refrigerator_Style:Side-by-Side%20Refrigerator:101048_03_Refrigerator_Style:French%20Door%20Refrigerator:101048_03_Refrigerator_Style:Four%20Door%20Refrigerator&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t3-v3.png?context=bWFzdGVyfHJvb3R8MTYzOTMzfGltYWdlL3BuZ3xoOTEvaDg3Lzk4OTYyOTk5MjE0MzgucG5nfDNhYTg4ZDcwOWIzYmQ5NDEwOGRkMDZiMGQ5MGUyMjkwNWI0Y2FlNDY3YTUzYWU4YjY0OWJlOTQ3OTQwNjM4ZDE",
+url:"https://www.reliancedigital.in/page/oneplus-tv?searchQuery=:price-asc:availability:Exclude%20out%20of%20Stock:brand:One%20Plus&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t4.png?context=bWFzdGVyfHJvb3R8MTkyMzMyfGltYWdlL3BuZ3xoNjMvaDU1Lzk4OTU0MjgxNjE1NjYucG5nfDIwMzg5MzgxYmQ0N2Q4Y2VkNjQzYWM5MjdlM2ZhZTMxOWViODBkZmFhYTFiMGM0Mjk1NmZhNzkwNGMwZTZlZjk",
+url:"https://www.reliancedigital.in/page/motorola-e32s-mobile"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t5-v3.png?context=bWFzdGVyfHJvb3R8MjM2NzM1fGltYWdlL3BuZ3xoM2EvaDY4Lzk4OTgzMzYwOTIxOTAucG5nfGQ2Y2VjNDY5YjZjYjdlZWRhMjNmMTgyZDBjN2M0Y2FhMjE2ZGY0MDRhYTBkODZkNmM0MzI5YTBmOTU1ODE2NmU",
+url:"https://www.reliancedigital.in/page/samsung-tv?searchQuery=:price-asc:availability:Exclude%20out%20of%20Stock:brand:Samsung&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t6.png?context=bWFzdGVyfHJvb3R8MTcwNzIwfGltYWdlL3BuZ3xoN2QvaDEzLzk4OTU0MjgyMjcxMDIucG5nfDg3YmY3Yzk3OGI1YzI4MzE0ZWM4OWZiMmIwMjVhNzA2MTU1ZmExZTU3ZGU1NjM0ZjM3MGEwZGRiZTU2NTliMWI",
+url:"https://www.reliancedigital.in/apple-mgn63hna-macbook-air-apple-m1-chip-8gb-256gb-ssd-macos-big-sur-retina-33-78-cm-13-3-inch-/p/491946461"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t7.png?context=bWFzdGVyfHJvb3R8MTc0ODY4fGltYWdlL3BuZ3xoZGYvaDI4Lzk4OTU0MjgyNTk4NzAucG5nfGNiNDhmZWJiYzhlM2QzZWM0OGYyM2FjYWZlY2JmNDE4ODQ3ZmU5NzkyY2I4NDMyNWJmN2MyZmFlNDRjMTQ1NTI",
+url:"https://www.reliancedigital.in/washing-machines/c/S101518?searchQuery=:relevance:availability:Exclude%20out%20of%20Stock:brand:Sansui&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t8.png?context=bWFzdGVyfHJvb3R8MTk2MjQ2fGltYWdlL3BuZ3xoNTQvaDNjLzk4OTU0MjgyOTI2MzgucG5nfGNmNzMwOWU5ZGIyOWZiMzlmNmE3MDgzM2Y5YjdhM2Y2ZjAwYzE3ZjYwNTYxMGUwMmNlNTVhZjhhOTY5MmRlMGQ",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-deals-smartwatches:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t9.png?context=bWFzdGVyfHJvb3R8MTkzMTYzfGltYWdlL3BuZ3xoOWUvaDY1Lzk4OTU0MjgzMjU0MDYucG5nfDQzMmZkNGI3YWE4YjNkNmE4NzRhNjgzMjQyNWE3ZWM1ZGNlYzQ1ZTY3ZDZjMzE2ZjAxYWM0ODgyNWE2YjdjMzY",
+url:"https://www.reliancedigital.in/hp-15s-fq5111tu-laptop-12th-gen-intel-core-i5-1235u-8-gb-ram-512-gb-ssd-windows-11-home-mso-fhd-39-6-cm-15-6-inch-/p/493177598"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t10.png?context=bWFzdGVyfHJvb3R8MTcxNTU5fGltYWdlL3BuZ3xoODkvaDVkLzk4OTU0MjgzNTgxNzQucG5nfDk1OTA4OTg0ZGY0YjNjMjgzZTdlODU0MmM5OTMyYjllOTE3YmQxMzllZjBkYjMxNzA2YTIxNWQ0MzMwNTc1Nzk",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-kitchen-appliances:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t11.png?context=bWFzdGVyfHJvb3R8MTUzNjI5fGltYWdlL3BuZ3xoNzcvaDNhLzk4OTU0MjgzOTA5NDIucG5nfGUzMmFlNTk4ZDI1ZGRkNWZlNmQxNzAwZTQ5ZjczZjU2OGRmMDhkMmFjMmM3ZjNjNjU4YWI1MjVkYTg1YTU1OTA",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-neckbands:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r2-t12.png?context=bWFzdGVyfHJvb3R8MTk2NTY0fGltYWdlL3BuZ3xoNmEvaGRjLzk4OTU0Mjg0MjM3MTAucG5nfDBkMWU3NGExZTk3ZTMzNjMwZjExN2RhY2UxOWZjOTFlYzhkOWU4M2IwNDdhYjI3YWNmNGU5YWZmZjhkOGRjZDg",
+url:"https://www.reliancedigital.in/canon-pixma-g3010-inktank-multi-function-color-wi-fi-printer/p/491419970"
+}]
+
+data.forEach(function(el){
+    let div=document.createElement("div")
+
+    let an=document.createElement("a")
+    an.href=el.url;
+
+    let img=document.createElement("img")
+    img.src=el.Image;
+
+    an.append(img)
+    div.append(an)
+    document.querySelector("#box1").append(div)
+})
+
+
+let data2=[
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t1-v2.png?context=bWFzdGVyfHJvb3R8NTU0NDl8aW1hZ2UvcG5nfGhiNS9oYzgvOTg5NjA5MjUzMjc2Ni5wbmd8MWQxYzEzNDc1ZDJjYWQwZDhiMDVjODFiYjRjYTEzOTE4ODZkZjMwY2U5MTJkZTMyYmVjY2JhMThiOTI3MTE3NA",
+url:"https://www.reliancedigital.in/search?q=:relevance:productTags:festival-of-electronics-brand-apple:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t2.png?context=bWFzdGVyfHJvb3R8NDk5NTR8aW1hZ2UvcG5nfGgwMC9oYWQvOTg5NTk3Mzg0NzA3MC5wbmd8NDY3NTczMGVlZjM4NDQ1MTVkYTRjMGZiNTA3ZDBlNWE5YzU5N2FlNGY1ZDlkYjE3ODkyZTFhN2U0NzA5NDExNQ",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-oneplus:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t3.png?context=bWFzdGVyfHJvb3R8NTExODN8aW1hZ2UvcG5nfGg0My9oNTIvOTg5NTk3Mzg3OTgzOC5wbmd8NDUzZmY1ZWE2NDhjNjY3MGMxNWYwZjlhYzc3ZDg5YjVhNGU1ODYzYzZiNDBmMDUwMzYxNTQzNmM1ZmM4YzZkYg",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-lifelong:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t4-v2.png?context=bWFzdGVyfHJvb3R8NTk5Mjd8aW1hZ2UvcG5nfGgxZS9oNDIvOTg5NjA5MjU2NTUzNC5wbmd8MWM5ZTk4ZDAxMmVhNGM2ZTIzZjQ4MzUwZGYyMGQ2ODUwMzkxYjg1MGU1MGQ5YzI4ZTVhMjQxMjYyNGMzMzdlMg",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-samsung:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t5-v2.png?context=bWFzdGVyfHJvb3R8NDQxMDN8aW1hZ2UvcG5nfGgwZS9oYjMvOTg5NjA5MjU5ODMwMi5wbmd8YWRlZThjNjMwN2ZmMzFlZTQzOTM0YWY3YTM3YWRmZTNkNDY5YWNmNDZkZTIyNGU3NzkyNzlhZTZiZWUzYTZhNA",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-ptron:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t6.png?context=bWFzdGVyfHJvb3R8Mzc1NzZ8aW1hZ2UvcG5nfGgzNC9oZDMvOTg5NTk3NzAyNTU2Ni5wbmd8NzE1NDhjOWIyZDg5OTEyNjFlZTUyNGM5OGIyMThiOWMxZWZhZWNmMzA3ZTdhMTdhNDc2NzJmMjkyODQ2ZTNkNw",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-noise:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t7.png?context=bWFzdGVyfHJvb3R8NDAzMTl8aW1hZ2UvcG5nfGhmNy9oMjEvOTg5NTk3NzA1ODMzNC5wbmd8NGMyMDAyYWE3NTYzNGU1NGEwMjk0NmZmZTI5ZWU0NDIzMGU0OTc3MGI0ZjIxZjUzYzMzYjc1NzQxMWIzNjA1Mg",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-hp:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r6-t8-v2.png?context=bWFzdGVyfHJvb3R8NDUyNzV8aW1hZ2UvcG5nfGhiYi9oOTEvOTg5NjA5MjYzMTA3MC5wbmd8ZWE4NjViOTNkYTEwZTE0Y2E5MDkyMjYxYjA1M2RhZTE0N2Y2MzhiNWZjNjI0ZmI5M2FhMDRhNzg3ODMwNDdkZQ",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-brand-boat:availability:Exclude%20out%20of%20Stock&page=0"
+}
+]
+data2.forEach(function(el){
+    let div=document.createElement("div")
+
+    let an=document.createElement("a")
+    an.href=el.url;
+
+    let img=document.createElement("img")
+    img.src=el.Image;
+
+    an.append(img)
+    div.append(an)
+    document.querySelector("#box2").append(div)
+})
+
+let data3=[{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t2.png?context=bWFzdGVyfHJvb3R8MTkwODM1fGltYWdlL3BuZ3xoMWYvaDllLzk4OTU5Nzc4MTE5OTgucG5nfDk3NjZlMmNjMTA1MTM3NDJmNGRiNDE1Y2E3NWI5MGRhMDQ1NWY5ZjMxOGY0NTgyNTdhNzhlMWQ3MmY3YzVlNmY",
+url:"https://www.reliancedigital.in/smart-phones/c/S101711?searchQuery=:relevance&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t3.png?context=bWFzdGVyfHJvb3R8MjExOTA4fGltYWdlL3BuZ3xoYjQvaDZjLzk4OTU5Nzc4NDQ3NjYucG5nfGQxNTdhM2ZlMDk1NDUxOWNkZDM3ZjMwYjMzOGUzNmNlOWVjOTA5ODZjZDc4MWI5MWExMmViMjQzZmRkYjk4ZGE",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-laptops:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t4.png?context=bWFzdGVyfHJvb3R8MjE1NDY1fGltYWdlL3BuZ3xoNzgvaDg4Lzk4OTU5Nzc4Nzc1MzQucG5nfGNmZDdiMTEzMGYzNDA2OTRkMjk2YTJkYjM2MmM5ZDBkZTZiNGYyYmNjOTIxYTU1Y2UzNTQyN2Q0OGMzY2JjNjk",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-home-appliances:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t5.png?context=bWFzdGVyfHJvb3R8MTc0MDM4fGltYWdlL3BuZ3xoNzkvaDFmLzk4OTU5Nzc5MTAzMDIucG5nfDFjNjIxMTEzYmNiMTJmNDY3OTBkZWU0Y2U3OWMwMzQ4YWQzMTk0ZTRkODFmMTI3NjI1NDIxNDJmMTc5ZjBkMGU",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-neckbands:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t6.png?context=bWFzdGVyfHJvb3R8MjAzMjE0fGltYWdlL3BuZ3xoYWUvaGEzLzk4OTU5Nzc5NDMwNzAucG5nfDU3ZDAzZmU1OTM3MjYzYmFkN2VlMTdkNzczYWJmODAwZDIwMmI1ZTRmNGQyYWQ3ZjNjODJlNDMxZjgyYmU5OTk",
+url:"https://www.reliancedigital.in/televisions/c/S101812?searchQuery=:price-asc:availability:Exclude%20out%20of%20Stock&page=0"
+},
+{
+Image:"https://www.reliancedigital.in/medias/festival-of-electronic-sale-r7-t1.png?context=bWFzdGVyfHJvb3R8MTk1MjI1fGltYWdlL3BuZ3xoNWMvaDBmLzk4OTU5Nzc3NzkyMzAucG5nfGEyZjNlYWQ5NDQyOTFkN2NhZjAyYTAzZDBjZjU1ZjY5NmI0MGYzZDdhMjFjNDM2Y2JmNDRlODJjMGE5YjAyMTk",
+url:"https://www.reliancedigital.in/search?q=:price-asc:productTags:festival-of-electronics-category-kitchen-appliances:availability:Exclude%20out%20of%20Stock&page=0"
+}
+]
+
+data3.forEach(function(el){
+    let div=document.createElement("div")
+
+    let an=document.createElement("a")
+    an.href=el.url;
+
+    let img=document.createElement("img")
+    img.src=el.Image;
+
+    an.append(img)
+    div.append(an)
+    document.querySelector("#box3").append(div)
+})
